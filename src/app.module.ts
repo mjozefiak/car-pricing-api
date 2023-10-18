971c9ad0de853972f1b082d4dcc03f3c9e905865
Mikołaj Józefiak <>
2023-10-18 14:52:42 +0200

    Created files for modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ReportController } from './report/report.controller';
+import { ReportService } from './report/report.service';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, ReportModule],
+  controllers: [AppController, ReportController],
+  providers: [AppService, ReportService],
 })
 export class AppModule {}
